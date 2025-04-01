e002ba1c40d237db405636a53abe3a0c500051dd
moad <moad2632005@gmail.com>
2025-04-01 13:36:06 +0000

    adding zip

--- build-linux.sh
+++ build-linux.sh
@@ -12,6 +12,10 @@ build_linux() {
     cp build/crist-project dist/linux/
     cp -r assets dist/linux/
     echo "Linux build completed. Output in dist/linux/"
+    # Create a ZIP archive of the Linux build
+    echo "Creating ZIP archive of Linux build..."
+    zip -r dist/linux.zip dist/linux
+    echo "ZIP archive created: dist/linux.zip"
 }
 
 build_linux
